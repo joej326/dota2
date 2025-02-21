e047c9c58d1312ebe49922626100c9d489e1bde2
Joseph Joyce <joej326@gmail.com>
2025-02-20 23:57:29 -0600

    added gofy gobers game

--- src/app/miscellaneous/the_[insert_name]_game.ts
+++ src/app/miscellaneous/the_[insert_name]_game.ts
@@ -1,5 +1,6 @@
 
 export const games = {
     carlos_mencia: 8175730351,
-    how_I_came_up_with_it_mi_amigos: 8175919261
+    how_I_came_up_with_it_mi_amigos: 8175919261,
+    gofy_gobers: 8183431318
 };
